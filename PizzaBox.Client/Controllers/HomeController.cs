0191645bac2aa7dfb44fd8bb9d96f655db7b058f
Akovi Mensah <abmensah@wisc.edu>
2021-05-06 04:47:11 -0500

    added models

--- PizzaBox.Client/Controllers/HomeController.cs
+++ PizzaBox.Client/Controllers/HomeController.cs
@@ -1,37 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using PizzaBox.Client.Models;
+using PizzaBox.Storage;
+using PizzaBox.Storage.Repositories;
 
 namespace PizzaBox.Client.Controllers
 {
-    public class HomeController : Controller
+  [Route("[controller]")]
+  public class HomeController : Controller
+  {
+    private readonly UnitOfWork _unitOfWork;
+
+    public HomeController(UnitOfWork unitOfWork)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _unitOfWork = unitOfWork;
     }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+      //ViewBag.Order = new OrderViewModel();
+      //ViewData
+      //TempData
+
+      var view = View("index", new OrderViewModel(_unitOfWork));
+
+      return view;
+    }
+  }
 }
